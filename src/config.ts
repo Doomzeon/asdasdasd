73c1df8958326bd49045b3a9448f4a09cbd9b650
doomzeon <osetskiivadim1@gmail.com>
2023-02-23 19:24:01 +0100

    s

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
+
+export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
+  WalletAdapterNetwork.Devnet) as WalletAdapterNetwork;
+// const network = WalletAdapterNetwork.Devnet;
+export const rpcHost =
+  process.env.NEXT_PUBLIC_RPC_HOST || clusterApiUrl(network);
+
+export const candyMachineId = new PublicKey(
+  process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
+    "3zwFR3spiwbSSMtvVKG2bRT6ttqFoC3MHCafGP8ZrdLz"
+);
+export const defaultGuardGroup =
+  process.env.NEXT_PUBLIC_DEFAULT_GUARD_GROUP || undefined; // undefined means default
+
+// "qasJ6jhgtngKk2QnEPdDjuFH8NMoM58W8TxPBXAChPY"
+// "3zwFR3spiwbSSMtvVKG2bRT6ttqFoC3MHCafGP8ZrdLz"
+// "DAA8yRLu7acVs3kxaTyCjoEjNWGinLaCKVhDY29ASNua"
+
+export const whitelistedWallets = [
+  "82Qix3YR8DXcmbRh9XwrUCz6jRJdpAqs1D8vd9pdyUNv",
+  "D3BavK82cowD84ivWxEDrEJV93sBkFspBa9sLhWJB4mj",
+  "FXoth7ba7LALmJGFaidCcMnhsxEagBDBDKCmkpr2QuDr",
+  "232Z5QNvQ4wRyraGWFpC5i3HEbqozEWgBCV95eWASaG1",
+  "D8ivzpXkG66VknRdVjEF1HjqS3T1tzcqraVR6FYaeLsV",
+  "sTAKERL4U8fbn2nUte6rPVdcrz2z3fEh1adQEDrGULr",
+];
